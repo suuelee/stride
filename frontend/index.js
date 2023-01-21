aea7eca24ea05c8b5ef7deb08834b8287bbe093f
HarinHaoWu <master.harin@gmail.com>
2023-01-21 14:16:48 -0800

    Redux Saga Setup

--- frontend/index.js
+++ frontend/index.js
@@ -5,5 +5,13 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppWrapper = () => (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+AppRegistry.registerComponent(appName, () => AppWrapper);
