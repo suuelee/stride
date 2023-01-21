3baf72f3a6df10c6b3e087ab7d69605504edb620
tsomani <tabreek.somani@gmail.com>
2023-01-21 13:40:24 -0800

    Backend structure initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from 'express'
+import profileRouter from './routes/profileRouter.js'
 
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/profile', profileRouter)
 
 app.listen(port, () => {
     console.log(`Stride backend listening on port ${port}`)
---
+++ backend/src/config/database.js
@@ -0,0 +1,7 @@
+// const mongoose = require("mongoose");
+//
+// const uri = "mongodb+srv://mongodb:stridepw@stride-cluster.vbr2nwk.mongodb.net/?retryWrites=true&w=majority";
+// mongoose.connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
