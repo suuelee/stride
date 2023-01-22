30a2a00b8adc61a32b711ea9636f7bf093ba2d34
Sue Lee <suuelee@outlook.com>
2023-01-22 11:18:41 -0800

    edit profile

--- frontend/src/screens/Profile.js
+++ frontend/src/screens/Profile.js
@@ -67,8 +67,8 @@ function Profile() {
               >Phone Number</Text>
               <Text
               style={{
-                color: 'black',
-                fontSize: 18,
+                fontWeight: 'bold',
+                fontSize: 15,
               }}
            >
                 123-456-7890
@@ -94,8 +94,8 @@ function Profile() {
             >Email</Text>
           <Text 
           style={{
-            color: 'black',
-            fontSize: 18,
+            fontWeight: 'bold',
+            fontSize: 15,
           }}
        
 >
@@ -112,7 +112,126 @@ function Profile() {
         width: '95%'
       }}>
 
-      <WalkerDescriptionCard></WalkerDescriptionCard>
+<View style={{width: '90%', height: 200, flexDirection: 'row'}}>
+      <View style={{flex: 1, justifyContent: 'space-around'}}>
+        <View style={{flex: 1}} />
+        <Image
+          source={require('../assets/Shirt.png')}
+          style={{
+            flex: 1,
+            width: 80,
+            resizeMode: 'contain',
+            alignSelf: 'center',
+            tintColor: 'green',
+          }}
+        />
+        <Image
+          source={require('../assets/Pants.png')}
+          style={{
+            flex: 1,
+            width: 80,
+            resizeMode: 'contain',
+            alignSelf: 'center',
+            tintColor: 'blue',
+          }}
+        />
+        <View style={{flex: 1}} />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            fontSize: 16,
+          }}>
+          Description
+        </Text>
+        <View
+          style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
+          <Image
+            source={require('../assets/Height.png')}
+            style={{
+              height: 18,
+              width: 18,
+              resizeMode: 'contain',
+              alignSelf: 'center',
+              marginRight: 5,
+            }}
+          />
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 12,
+            }}>
+            {"5’9’’"}
+          </Text>
+        </View>
+        <View
+          style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
+          <Image
+            source={require('../assets/Hair.png')}
+            style={{
+              height: 18,
+              width: 18,
+              resizeMode: 'contain',
+              alignSelf: 'center',
+              marginRight: 5,
+            }}
+          />
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 12,
+            }}>
+            {"Caucasian"}
+          </Text>
+        </View>
+        <View
+          style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
+          <Image
+            source={require('../assets/Ethnicity.png')}
+            style={{
+              height: 18,
+              width: 18,
+              resizeMode: 'contain',
+              alignSelf: 'center',
+              marginRight: 5,
+            }}
+          />
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 12,
+            }}>
+            {"Caucasian"}
+          </Text>
+        </View>
+        <View
+          style={{flexDirection: 'row', marginTop: 15, alignItems: 'center'}}>
+          <Image
+            source={require('../assets/Gender.png')}
+            style={{
+              height: 18,
+              width: 18,
+              resizeMode: 'contain',
+              alignSelf: 'center',
+              marginRight: 5,
+            }}
+          />
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 12,
+            }}>
+            {"Male"}
+          </Text>
+        </View>
+      </View>
+    </View>
 
 
       </View>
