2395133a12a133fdcdd54fae7a020d0570769ae9
erickexinlu <erickexinlu@gmail.com>
2023-01-21 16:47:06 -0800

    refine file structure and create schemas to reuse for models

--- backend/src/controllers/profileController.js
+++ backend/src/controllers/profileController.js
@@ -1,23 +1,28 @@
+// import mongoose from "../config/database.js";
 
-import mongoose from "../config/database.js";
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+var profileSchema = new Schema({
+    _id: { type: String },
+    email: { type: String },
+    password: { type: String },
+    fName: { type: String },
+    lName: { type: String },
+    isStrider: { type: String },
+    defaultAddress: { type: String },
+    noTripsCompleted: { type: String },
+    hobbies: { type: String }
+});
+
+const Profile = mongoose.model('Profile', profileSchema);
+
 
 export default class ProfileController {
     saveProfile(req) {
         return new Promise((resolve, reject) => {
 
-            const Profile = mongoose.model("Profile", {
-                _id:  {type: String},
-                email: {type: String},
-                password: {type: String},
-                fName: {type: String},
-                lName: {type: String},
-                isStrider: {type: String},
-                defaultAddress: {type: String},
-                noTripsCompleted: {type: String},
-                hobbies: {type: String}
-            })
-
-            const profile = new Profile({
+            const new_profile = new Profile({
                 _id: req.body.id, // phone number
                 email: req.body.email,
                 password:  req.body.password,
@@ -29,7 +34,7 @@ export default class ProfileController {
                 hobbies: req.body.hobbies
             });
 
-            profile.save((err, res) => {
+            new_profile.save((err, res) => {
                 if (err) {
                     console.log(err);
                     reject(err);
@@ -37,7 +42,6 @@ export default class ProfileController {
                     console.log("Resolving the promise");
                     resolve(res);
                 }
-
             });
         });
     }
