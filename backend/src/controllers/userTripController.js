7fcc557ca370c66877eb03d2d57ee25ffda54de3
erickexinlu <erickexinlu@gmail.com>
2023-01-22 00:34:06 -0800

    hotfix some unbalanced brackets

--- backend/src/controllers/userTripController.js
+++ backend/src/controllers/userTripController.js
@@ -186,118 +186,119 @@ export default class UserTripController {
       });
     });
   }
-}
-=======
-    loadLocation(req) {
-        return new Promise((resolve, reject) => {
-            console.log("Id is "+ req.body.userId);
-            UserTrip.findOne(({ userId: req.body.userId }), function (err, res) {
-                if (err) {
-                    reject(err);
-                } else {
-                    console.log("Successfully found!");
-                    resolve(res.pickupAddress);
-                }});
-        });
+
+
+  loadLocation(req) {
+    return new Promise((resolve, reject) => {
+      console.log("Id is " + req.body.userId);
+      UserTrip.findOne(({ userId: req.body.userId }), function (err, res) {
+        if (err) {
+          reject(err);
+        } else {
+          console.log("Successfully found!");
+          resolve(res.pickupAddress);
+        }
+      });
+    });
+  }
+
+
+
+
+  saveLocation(req) {
+    return new Promise((resolve, reject) => {
+      const new_trip = new UserTrip({
+        _id: req.body.id,
+        userId: req.body.userId,
+        striderId: req.body.striderId,
+        pickupAddress: req.body.pickupAddress,
+        dropoffAddress: req.body.dropoffAddress,
+        status: req.body.status,
+        createdTime: req.body.createdTime,
+        pickupTime: req.body.pickupTime,
+        dropoffTime: req.body.dropoffTime
+      });
+
+      new_trip.save((err, result) => {
+        if (err) {
+          reject({ error: err });
+        }
+        resolve(result);
+      });
+    });
+  }
+
+
+  endTrip(req) {
+    const UserTripModel = new UserTrip();
+
+    const trip = {
+      userId: req.body.userId,
+      tripId: req.body.tripId
     }
+    UserTripModel.finishTrip(trip, (err, result) => {
+      if (err) {
+        reject({ error: err });
+      }
+      resolve(result);
+    });
 
 
+  }
+
+  saveStrideLocation(req) {
+    return new Promise((resolve, reject) => {
+      const UserTripModel = new UserTrip();
+
+      const walker = {
+        userId: req.body.userId,
+        location: req.body.location,
+      };
+
+      UserTripModel.putStrideLocation(walker, (err, result) => {
+        if (err) {
+          reject({ error: err });
+        }
+        resolve(result);
+      });
+    });
 
 
-    saveLocation(req) {
-        return new Promise((resolve, reject) => {
-            const new_trip = new UserTrip({
-                _id: req.body.id,
-                userId: req.body.userId,
-                striderId: req.body.striderId,
-                pickupAddress: req.body.pickupAddress,
-                dropoffAddress: req.body.dropoffAddress,
-                status: req.body.status,
-                createdTime: req.body.createdTime,
-                pickupTime: req.body.pickupTime,
-                dropoffTime: req.body.dropoffTime
-            });
+  }
 
-            new_trip.save((err, result) => {
-                if (err) {
-                    reject({ error: err });
-                }
-                resolve(result);
-            });
-        });
-    }
+  getStrideLocation(req) {
+    return new Promise((resolve, reject) => {
+      const UserTripModel = new UserTrip();
+
+      const walker = {
+        userId: req.body.userId,
+      };
+
+      UserTripModel.getStrideLocation(walker, (err, result) => {
+        if (err) {
+          reject({ error: err });
+        }
+        resolve(result);
+      });
+    });
 
 
-    endTrip(req) {
-        const UserTripModel = new UserTrip();
+  }
 
-        const trip = {
-                userId: req.body.userId,
-                tripId: req.body.tripId
-            }
-        UserTripModel.finishTrip(trip, (err, result) => {
-            if (err) {
-                reject({ error: err });
-            }
-            resolve(result);
-        });
+  getAllPendingRequests(req) {
+    // get all pending walkers
+    return new Promise((resolve, reject) => {
+      // const UserTripModel = new UserTrip();
 
-
-}
-
-    saveStrideLocation(req) {
-        return new Promise((resolve, reject) => {
-            const UserTripModel = new UserTrip();
-
-            const walker = {
-                userId: req.body.userId,
-                location: req.body.location,
-            };
-
-            UserTripModel.putStrideLocation(walker, (err, result) => {
-                if (err) {
-                    reject({ error: err });
-                }
-                resolve(result);
-            });
-        });
-
-
-    }
-
-    getStrideLocation(req) {
-        return new Promise((resolve, reject) => {
-            const UserTripModel = new UserTrip();
-
-            const walker = {
-                userId: req.body.userId,
-            };
-
-            UserTripModel.getStrideLocation(walker, (err, result) => {
-                if (err) {
-                    reject({ error: err });
-                }
-                resolve(result);
-            });
-        });
-
-
-    }
-
-    getAllPendingRequests(req){
-        // get all pending walkers
-        return new Promise((resolve, reject) => {
-            // const UserTripModel = new UserTrip();
-
-            UserTripModel.find(({status: "pending"}), function (err, res) {
-                if (err) {
-                    console.log(err);
-                    reject(err);
-                } else {
-                    console.log("resolving this promise !!");
-                    resolve(res);
-                }
-            })
-        });
-    }
+      UserTripModel.find(({ status: "pending" }), function (err, res) {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          console.log("resolving this promise !!");
+          resolve(res);
+        }
+      })
+    });
+  }
 }
